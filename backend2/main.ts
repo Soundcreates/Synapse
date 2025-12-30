5c569d249bc9f135a592679da0951dfcdfbd68c5
Soundcreates <shantanav7@gmail.com>
2025-12-31 01:18:26 +0530

    some changes

--- backend2/main.ts
+++ backend2/main.ts
@@ -12,7 +12,7 @@ const app = express();
 // CORS configuration - must be before other middleware
 const allowedOrigins = [
   "http://localhost:3000",
-
+  "http://localhost:3001",
   "http://localhost:5173",
   "http://localhost:5000",
   "https://synapse-dusky.vercel.app/",
