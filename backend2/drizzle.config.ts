ad41088589f67f70b42bc2d75b5a684fa7c98e0f
Soundcreates <shantanav7@gmail.com>
2026-01-04 13:07:21 +0530

    Fixing db issues pt3

--- backend2/drizzle.config.ts
+++ backend2/drizzle.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "drizzle-kit";
+import "dotenv/config";
 
 export default {
   schema: "./models/DataSetModel.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
+    url: process.env.DATABASE_URL!,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
   },
 } satisfies Config;
