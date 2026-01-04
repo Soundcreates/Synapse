165e146ef9c76ea9fda3dbb455b8683d194145c5
Soundcreates <shantanav7@gmail.com>
2026-01-04 12:44:18 +0530

    Minor fixes in client side plus updated build

--- client3/utils/baseUrl.ts
+++ client3/utils/baseUrl.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = axios.create({
-  baseURL: "https://synapse-backend-3rpa.onrender.com",
+  baseURL: "https://synapse-backend-3rpa.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
